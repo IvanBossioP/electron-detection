bd28be0827378d692b0791ad868903b466344551
IvanBossioP <71456648+IvanBossioP@users.noreply.github.com>
2022-05-29 11:36:35 +0200

    added two new checks and description

--- detect.js
+++ detect.js
@@ -27,8 +27,6 @@ const speechSyntesisCheck = () => new Promise(r => {
     speechSynthesis.getVoices()
     setTimeout(() => {
         const voices = speechSynthesis.getVoices()
-
-        console.log(voices)
         r(!voices.some(voice => voice.voiceURI.startsWith('Google')))
     }, 1000)
     
@@ -45,6 +43,13 @@ const webWorkerCheck = () => new Promise(r => {
         worker.terminate();
     })
 })
+const chromeCheck = () => {
+    return (!window.chrome) || (!chrome.app) || (typeof chrome.csi !== 'function') || (typeof chrome.loadTimes !== 'function')
+}
+
+const userDataCheck = () => {
+    return (!navigator.userAgentData) || navigator.userAgentData.brands.length === 0 || (!navigator.platform)
+}
 
 
 const tests = [
@@ -55,6 +60,8 @@ const tests = [
     {name: 'LanguageCheck', fn: languageCheck, weight: 'mid'},
     {name: 'UserAgentCheck', fn: userAgentCheck, weight: 'high'},
     {name: 'WebWorkerCheck', fn: webWorkerCheck, weight: 'high'},
+    {name: 'ChromeCheck', fn: chromeCheck, weight: 'high'},
+    {name: 'UserDataCheck', fn: userDataCheck, weight: 'mid'}
 
 ]
 Promise.all(tests.map(async ({name, fn, weight}) => {
